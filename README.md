d936d9489ae0d5585272ae20d1bc46c982df3c23
David Clark <kc0osh@gmail.com>
2022-08-30 21:00:49 -0500

    Adding challenge files

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# surfs_up
+# Surfs Up Challenge
+
+## Analysis Overview
+
+This project compares the weather for two months for the island of Oahu in Hawaii. This is the location where an investor is considering an investment in a new surf shop business. The investor is concerned about the weather and asked that weather data for Oahu be analyzed to determine if this is the best place to open a new surf shop. 
+
+For this purpose, all temperature data for the months of June and December will be extracted from the provided dataset. The data will then be used to calculate summary statistics for each month individually. The summary statistics will be compared and used to help the investor make a decision.
+
+## Results
+
+* The total count of data shows there were 183 fewer measurements for the month of December than for January.
+* The temperature readings for the months of June and January are remarkably similar when comparing the Average (Mean), Minimum and Maximum temperature readings.
+* As can be seen in the included tables. The difference in the mean temperatures is just 3.9 degrees.
+* The minimum reading showed the largest difference between the two months at 8 degrees.
+
+## Summary
+
+In summary, the selected location appears to be a good location to open a new surf shop. The weather will of course affect the business at different times both positively and negatively. However, based on the analysis performed the weather on Oahu appears to be a good location. If more data were available additional queries and analysis can be completed to help the decision making process.
+
+For example, analyzing wave data and people traffic at local beaches are two additional queries that could be helpful.
+
+* Waves - Wave data is not available in the current data set. If a wave dataset is available it would be good to do an analyis to compare the two months as we did with temperature.
+* People Analysis - Is there data on beach attendance at the beach where the Surf shop is planned? If attendance dataset is available then analyzing and comparing how many people visit the beach each 
--- app.py
+++ app.py
@@ -1,14 +1,92 @@
-# Import Dependencies
-from flask import Flask
+# Filename: app.py
+# Date: 08/28/2022
+# Module 9.5
 
-# Create a new Flask app instance
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+# Set up the database engine
+engine = create_engine('sqlite:///hawaii.sqlite',
+                connect_args={"check_same_thread": False})
+
+# Reflect the DB into our classes
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Save references to each db table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session link from Python to the database.
+session = Session(engine)
+
+# Set up Flask
 app = Flask(__name__)
 
-# Create Flask Routes
+# Define Welcome route
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def welcome():
+    return(
+    f"<h3>Welcome to the Climate Analysis API!</h3>"
+    f"Available Routes:<br/>"
+    f"/api/v1.0/precipitation<br/>"
+    f"/api/v1.0/stations<br/>"
+    f"/api/v1.0/tobs<br/>"
+    f"/api/v1.0/temp/start/end<br/>"
+    )
 
-@app.route('/hello')
-def hello():
-    return '<h1>A Big Hello!!</h1>'
+# Define the precipitation route
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+# Define the stations route
+@app.route('/api/v1.0/stations')
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+# Define the Monthly Temperature route
+@app.route('/api/v1.0/tobs')
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps = temps)
+
+# Define the statistics route
+@app.route('/api/v1.0/temp/<start>')
+@app.route('/api/v1.0/temp/<start>/<end>')
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    # Determine starting & ending date
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps = temps)
+
+    # Caculate the temp min, avg, and max
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
+if __name__ == "__main__":
+    app.run(debug=True)
